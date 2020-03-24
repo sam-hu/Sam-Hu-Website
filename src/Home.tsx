c0e2b7a4dbca969fe77b99c0121b4d5fb6f2e477
Sam Hu <samhu@yahoo.com>
2020-03-24 16:17:47 -0400

    timeline WIP

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Home.scss';
-
 import { spotifyData } from './spotify';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 // import { recommends } from './recommends';
+import {ReactComponent as BirthLogo} from "./birth.svg"
 
 const spotifyStyle = {
     fontSize: 48 + 'px',
@@ -41,8 +43,8 @@ const Home = function Home(props: { start: Function }) {
         </header>
         <div id="spotify">
             <div className="spotifyHeader">
-                <h2>Find Me On {spotify}</h2>
-                {/* <p>Or just check out what I’ve been listening to lately</p> */}
+                <h2>Here's what I listen to on {spotify}</h2>
+                {/* <p>It's where I spend all my time</p> */}
             </div>
             <ul>
                 {spotifyData.items.map((artist, i) => {
@@ -55,6 +57,109 @@ const Home = function Home(props: { start: Function }) {
                     }
                 })}
             </ul>
+        </div>
+        <div id="timeline">
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    // contentStyle={{ width: '400px', marginLeft: '80px'}}
+                    // contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                    date="1998"
+                    iconStyle={{ background: 'rgb(255, 255, 255)', color: '#fff' }}
+                    icon={<BirthLogo viewBox="0 0 150 150" />}
+                >
+                    <h3 className="vertical-timeline-element-title">Born in Hefei, China</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2001"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    // icon={<WorkIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Moved to the United States</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2004"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<BirthLogo viewBox="0 0 150 150" />}
+                >
+                    <h3 className="vertical-timeline-element-title">Settled down in Potomac, Maryland</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2006"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    // icon={React.createElement(img)}
+                >
+                    <h3 className="vertical-timeline-element-title">Started learning how to play piano</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2009"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Graduated from elementary school</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Circa 2010"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Forgot how to play piano</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2012"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Graduated from middle school</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2012"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">First certification as a USSF soccer referee</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2015"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Took my first coding class (shoutout to AP Comp Sci)</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2016"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Graduated from high school</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2019"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Graduated from Cornell University</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2020"
+                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    // icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">Made my first website</h3>
+                    <p>Congrats on making it to the end!</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
         </div>
         {/* <div id="recommends">
             <h2>Ryan <br />Recommends</h2>
