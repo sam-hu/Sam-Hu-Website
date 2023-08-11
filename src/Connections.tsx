1c1e7f9ea0f5db1cdbe668efd64412960ceaa58f
Sam Hu <samhu@yahoo.com>
2023-08-10 22:04:31 -0400

    More stuff

--- src/Connections.tsx
+++ src/Connections.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useLocation, useNavigate } from 'react-router-dom';
 import Papa from 'papaparse';
 import { DownloadOutlined, BackwardOutlined, CopyOutlined } from '@ant-design/icons';
+import Title from "antd/es/typography/Title";
 
 type ConnectionCategory = {
     description: string;
@@ -13,7 +14,8 @@ type ConnectionCategory = {
 
 export type ConnectionCategories = ConnectionCategory[];
 
-const correctFontSize = (str: string, elementWidth: number) => {
+const correctFontSize = (str: string, elementWidth?: number) => {
+    if (!elementWidth) elementWidth = isMobile() ? 50 : 200;
     const originalSize = 16;
     const length = str.length;
     const mult = elementWidth / (originalSize * length);
@@ -22,6 +24,39 @@ const correctFontSize = (str: string, elementWidth: number) => {
     return Math.round(fontSize);
 };
 
+const minChangesToEqualLists = (list1: string[], list2: string[]): number => {
+    const freq1: { [key: string]: number } = {};
+
+    // Count the frequency of strings in list1
+    for (const string of list1) {
+        freq1[string] = (freq1[string] || 0) + 1;
+    }
+
+    // Calculate the total changes needed
+    let changesNeeded = 0;
+
+    // Iterate through the strings in list2
+    for (const string of list2) {
+        if (freq1[string] && freq1[string] > 0) {
+            freq1[string] -= 1; // Mark the string as used
+        } else {
+            changesNeeded += 1; // Count strings that need to be changed
+        }
+    }
+
+    return changesNeeded;
+}
+
+const calcOffBy = (words: string[][], guess: string[]): number => {
+    let offs = []
+    for (const word of words) {
+        offs.push(minChangesToEqualLists(word, guess));
+    }
+    return Math.min(...offs);
+}
+
+const isMobile = () => window.innerWidth < 768;
+
 const Box = ({ word, selected, solved, onClick }: { word: string, selected: boolean, solved: boolean, onClick: () => void }) => {
     return <Button
         className="word-box"
@@ -29,7 +64,7 @@ const Box = ({ word, selected, solved, onClick }: { word: string, selected: bool
         type={selected ? "primary" : "default"}
         disabled={solved}
         style={{
-            fontSize: correctFontSize(word, 50),
+            fontSize: correctFontSize(word, isMobile() ? 50 : 75),
         }}>
         {word}
     </Button>
@@ -37,7 +72,7 @@ const Box = ({ word, selected, solved, onClick }: { word: string, selected: bool
 
 type WordState = {
     solved: boolean;
-    index: number;
+    difficulty: number; // 0, 1, 2, 3
 }
 
 type RecordedGuess = {
@@ -53,12 +88,14 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
     const sortedConnections = connections.sort((a, b) => a.id - b.id).map((c) => ({ ...c, words: c.words.map((w) => w.toUpperCase()) }));
     const allWords: { [key: string]: WordState } = {};
     const wordArr: string[] = [];
+    const groupedWords: string[][] = [[], [], [], []];
     for (let i = 0; i < sortedConnections.length; i++) {
         const connection = sortedConnections[i];
         connection.id = i; // normalize to 0, 1, 2, 3
         for (const word of connection.words) {
-            allWords[word] = { solved: false, index: connection.id };
+            allWords[word] = { solved: false, difficulty: connection.id };
             wordArr.push(word);
+            groupedWords[i].push(word);
         }
     }
 
@@ -78,35 +115,39 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
     const navigate = useNavigate();
 
     const checkIfSolved = () => {
-        if (selectedWords.length === 4) {
-            const firstWord = selectedWords[0];
-            const index = wordState[firstWord].index;
-            const solved = selectedWords.every(word => wordState[word].index === index);
-
-            const newMap = { ...wordState };
-            if (solved) {
-                for (const word of selectedWords) {
-                    newMap[word].solved = true;
-                    setWordState(newMap);
-                }
-                const newCategoryState = { ...categoriesState };
-                newCategoryState[index].solved = true;
-                setCategoriesState(newCategoryState);
-                console.log("solved");
-                if (newCategoryState[index].solved) {
-                    const allSolved = Object.values(newCategoryState).every(category => category.solved);
-                    if (allSolved) {
-                        setVictory(true);
-                        setShowModal(true);
-                    }
-                }
-            } else {
-                console.log("reset");
-            }
-
-            setGuesses([...guesses, { words: selectedWords, correct: solved, off: 0 }]);
-            setSelectedWords([]);
+        if (selectedWords.length !== sortedConnections.length) {
+            return;
         }
+
+
+        const firstWord = selectedWords[0];
+        const index = wordState[firstWord].difficulty;
+        const solved = selectedWords.every(word => wordState[word].difficulty === index);
+
+        const newMap = { ...wordState };
+        if (solved) {
+            for (const word of selectedWords) {
+                newMap[word].solved = true;
+                setWordState(newMap);
+            }
+            const newCategoryState = { ...categoriesState };
+            newCategoryState[index].solved = true;
+            setCategoriesState(newCategoryState);
+            if (newCategoryState[index].solved) {
+                const allSolved = Object.values(newCategoryState).every(category => category.solved);
+                if (allSolved) {
+                    setVictory(true);
+                    setShowModal(true);
+                }
+            }
+        } else {
+            console.log("reset");
+        }
+
+        setGuesses([...guesses, {
+            words: selectedWords, correct: solved, off: calcOffBy(groupedWords, selectedWords)
+        }]);
+        setSelectedWords([]);
     }
 
     const serializeAndDownloadCSV = () => {
@@ -136,19 +177,21 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
     }
 
     return (
-        <div style={{ padding: "48px calc(20px + (100vw - 400px) * 0.3)", paddingBottom: "144px" }}>
+        <div style={{ padding: "36px calc(20px + (100vw - 400px) * 0.3)", paddingBottom: "144px" }}>
             {guesses.length > 0 && <VictoryModal guesses={guesses} allWords={allWords} visible={victory && showModal} onClose={() => setShowModal(false)} />}
+
             {
-                Object.entries(categoriesState).map(([key, value]) => {
-                    if (!value.solved) {
+                guesses.map((guess, index) => {
+                    if (!guess.correct) {
                         return null;
                     }
 
+                    const category = categoryMap[allWords[guess.words[0]].difficulty];
                     return (
                         <div
                             style={{
                                 color: 'white',
-                                backgroundColor: colorsByDifficulty[value.id],
+                                backgroundColor: colorsByDifficulty[category.id],
                                 borderRadius: "8px",
                                 display: "flex",
                                 justifyContent: "center",
@@ -157,15 +200,15 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
                                 height: "72px",
                                 margin: "0 0 8px",
                             }}
-                            key={key}>
-                            <div>{value.description}</div>
-                            <div style={{ fontSize: correctFontSize(value.words.join(", "), 200) }}>{value.words.join(", ")}</div>
+                            key={index}>
+                            <div>{category.description}</div>
+                            <div style={{ fontSize: correctFontSize(guess.words.join(", "), 200) }}>{guess.words.join(", ")}</div>
                         </div>
                     )
                 })
             }
 
-            <div style={{ display: 'grid', gridTemplateColumns: "1fr 1fr 1fr 1fr", margin: "0 -4px 36px -4px" }}>
+            <div style={{ display: 'grid', gridTemplateColumns: "1fr 1fr 1fr 1fr", margin: "0 -4px 24px -4px" }}>
                 {
                     wordOrder.map((word, index) => {
                         if (wordState[word].solved) {
@@ -184,7 +227,7 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
             </div>
 
             {!victory
-                && <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", marginBottom: '24px' }} >
+                && <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", marginBottom: '12px' }} >
                     <Button className="button with-margin" type="primary" onClick={() => checkIfSolved()} disabled={selectedWords.length !== 4}>
                         Submit
                     </Button>
@@ -210,6 +253,7 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
                 bottom: 0,
                 left: 0,
                 padding: "8px calc(20px + (100vw - 400px) * 0.3)",
+                paddingTop: "12px",
                 width: "100%",
                 backgroundColor: "white",
             }}>
@@ -233,33 +277,37 @@ export const WordsContainer = ({ connections }: { connections: ConnectionCategor
                     {copied ? "Copied!" : "Copy link"}
                 </Button>
                 <div style={{ display: "flex", justifyContent: "center", gap: "12px" }}>
-                    <Button className="button with-margin" onClick={() => navigate("/connections-create", { state: { categories: sortedConnections } })} icon={<BackwardOutlined />}>
+                    <Button className="button" onClick={() => navigate("/connections-create", { state: { categories: sortedConnections } })} icon={<BackwardOutlined />}>
                         Back to create
                     </Button>
 
-                    <Button className="button with-margin" onClick={() => serializeAndDownloadCSV()} icon={<DownloadOutlined />}>
+                    <Button className="button" onClick={() => serializeAndDownloadCSV()} icon={<DownloadOutlined />}>
                         Export as CSV
                     </Button>
                 </div>
             </div>
 
             {/* Guesses */}
+            {guesses.length > 0 && <Title level={5} style={{ display: "flex", justifyContent: "center" }}>
+                Guesses
+            </Title>}
             {
                 guesses.map((guess, index) => <div
                     key={index}
                     style={{
                         display: 'flex',
-                        justifyContent: 'center',
+                        justifyContent: 'space-between',
                         alignItems: 'center',
-                        color: guess.correct ? 'green' : 'gray',
+                        backgroundColor: guess.correct ? colorsByDifficulty[allWords[guess.words[0]].difficulty] : 'gray',
                         border: `1px solid`,
                         borderRadius: "8px",
                         height: '36px',
                         marginBottom: '8px',
-                        fontSize: correctFontSize(guess.words.join(", "), 200),
+                        color: 'white',
                     }}
                 >
-                    {guess.words.join(", ")}
+                    <span style={{ margin: "0 24px", fontSize: correctFontSize(guess.words.join(", "), isMobile() ? 150 : 200) }}>{guess.words.join(", ")}</span>
+                    <span style={{ paddingRight: "24px", minWidth: "84px", display: "flex", justifyContent: "right" }}> {guess.off > 0 ? `Off by ${guess.off}` : "Correct!"}</span>
                 </div>)
             }
         </div >
@@ -328,7 +376,7 @@ export const Test = () => {
             {
                 description: "test3",
                 id: 3,
-                words: ["test9", "test10", "Reminisce", "Californiacation"]
+                words: ["AAAAAAAAAAAAAA", "ABRACADABRA", "Reminisce", "Californiacation"]
             },
             {
                 description: "test4",
@@ -342,7 +390,7 @@ export const Test = () => {
 }
 
 const VictoryModal = ({ guesses, allWords, visible, onClose }: { guesses: RecordedGuess[], allWords: { [key: string]: WordState }, visible: boolean, onClose: () => void }) => {
-    const guessList = guesses.map((guess) => guess.words.map((word) => iconsByDifficulty[allWords[word].index]).join(" "));
+    const guessList = guesses.map((guess) => guess.words.map((word) => iconsByDifficulty[allWords[word].difficulty]).join(" "));
 
     const footer = <div style={{ display: "flex", flexDirection: "column", marginTop: "24px" }}>
         <Button className="button with-margin" onClick={() => navigator.clipboard.writeText(guessList.join("\n"))}>
