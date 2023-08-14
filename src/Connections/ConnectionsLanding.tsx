f160cba5a62fed278b5c2ca7b1acdab30bcdde1b
Sam Hu <samhu@yahoo.com>
2023-08-14 03:18:37 -0400

    one more time

--- src/Connections/ConnectionsLanding.tsx
+++ src/Connections/ConnectionsLanding.tsx
@@ -3,7 +3,7 @@ import { Button, Modal } from "antd";
 import Title from "antd/es/typography/Title";
 import { useNavigate } from "react-router-dom";
 import { CaretRightOutlined, EditOutlined, BookOutlined, QuestionCircleOutlined } from '@ant-design/icons';
-import { colorsByDifficulty } from "./ConnectionsPlay";
+import { colorsByDifficulty, isMobile } from "./ConnectionsPlay";
 import { ConnectionsMenu } from "./ConnectionsMenu";
 
 export const ConnectionsLanding = () => {
@@ -20,7 +20,7 @@ export const ConnectionsLanding = () => {
                         <img src="/Connections.svg" height="36px" width="36px" />
                         <Title level={1} style={{ marginTop: 0, marginBottom: 0, marginLeft: "12px" }}>Connections</Title>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "70vh", maxHeight: "516px" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "70vh", maxHeight: isMobile() ? "500px" : "1000px" }}>
                         <Button
                             className="button with-margin"
                             style={{ height: "96px" }}
