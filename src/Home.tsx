74e585cc28f83f25c713f40302774b67be800111
Sam Hu <samhu@yahoo.com>
2023-09-08 19:38:15 -0400

    remove helmet from home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet';
 import spotifyData from './Spotify/spotify';
 import MyTimeline from './Timeline/timeline';
 import './Home.scss';
@@ -10,76 +9,68 @@ const spotify = <a style={{ color: '#1DB954' }} className="spotifyText link" tar
 
 function Home() {
   return (
-    <>
-      <Helmet>
-        <title>Sam Hu</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <meta property="og:title" content="Sam Hu" />
-        <meta property="og:url" content="https://sam-hu.com" />
-      </Helmet>
-      <div id="home">
-        <div id="topCurve" />
-        <header>
-          <div id="info">
-            <h1>Hey, I'm Sam.</h1>
-            <p>
-              I'm currently at&nbsp;
-              {found}
-              &nbsp;
-              building banking for the self-employed.
-              Previously, I was a software engineer at&nbsp;
-              {okta}
-              .
-              Before that, I studied computer science and economics at Cornell University.
-              In my free time I like to ski, snowboard, listen to hip hop, and watch soccer.
-            </p>
-            <ul>
-              <li><a target="_blank" rel="noopener noreferrer" href="/Sam-Hu-Resume.pdf">Resume</a></li>
-              <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sam-hu">LinkedIn</a></li>
-              <li><a href="/connections">Play a game</a></li>
+    <div id="home">
+      <div id="topCurve" />
+      <header>
+        <div id="info">
+          <h1>Hey, I'm Sam.</h1>
+          <p>
+            I'm currently at&nbsp;
+            {found}
+            &nbsp;
+            building banking for the self-employed.
+            Previously, I was a software engineer at&nbsp;
+            {okta}
+            .
+            Before that, I studied computer science and economics at Cornell University.
+            In my free time I like to ski, snowboard, listen to hip hop, and watch soccer.
+          </p>
+          <ul>
+            <li><a target="_blank" rel="noopener noreferrer" href="/Sam-Hu-Resume.pdf">Resume</a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sam-hu">LinkedIn</a></li>
+            <li><a href="/connections">Play a game</a></li>
+          </ul>
+        </div>
+        <div id="aside">
+          <img src="/Sam-Hu.png" id="profilePic" alt="Sam Hu" />
+        </div>
+      </header>
+      <div id="spotify">
+        <div className="spotifyHeader">
+          <h2>
+            Here's what I listen to on&nbsp;
+            {spotify}
+          </h2>
+        </div>
+        <div id="musicContainer">
+          <div className="artistContainer">
+            <ul className="artistClass">
+              {spotifyData.items.filter((_, i) => document.body.clientWidth > 550 || i < 12).map((artist) => (
+                <li className="artist" key={artist.id}>
+                  <a target="_blank" rel="noopener noreferrer" href={artist.uri}>
+                    <img src={artist.images[1].url} alt="" />
+                    {artist.name}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
-          <div id="aside">
-            <img src="/Sam-Hu.png" id="profilePic" alt="Sam Hu" />
-          </div>
-        </header>
-        <div id="spotify">
-          <div className="spotifyHeader">
-            <h2>
-              Here's what I listen to on&nbsp;
-              {spotify}
-            </h2>
-          </div>
-          <div id="musicContainer">
-            <div className="artistContainer">
-              <ul className="artistClass">
-                {spotifyData.items.filter((_, i) => document.body.clientWidth > 550 || i < 12).map((artist) => (
-                  <li className="artist" key={artist.id}>
-                    <a target="_blank" rel="noopener noreferrer" href={artist.uri}>
-                      <img src={artist.images[1].url} alt="" />
-                      {artist.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <iframe
-              title="my playlist"
-              className="playlist"
-              src="https://open.spotify.com/embed/playlist/7dSBjc6YMggxd0IlgvCV9j"
-              width="380"
-              height="650"
-              allow="encrypted-media"
-            />
-          </div>
+          <iframe
+            title="my playlist"
+            className="playlist"
+            src="https://open.spotify.com/embed/playlist/7dSBjc6YMggxd0IlgvCV9j"
+            width="380"
+            height="650"
+            allow="encrypted-media"
+          />
         </div>
-        <div id="timeline">
-          <h2>My timeline</h2>
-          <MyTimeline />
-        </div>
-        <footer>Sam Hu © 2023</footer>
       </div>
-    </>
+      <div id="timeline">
+        <h2>My timeline</h2>
+        <MyTimeline />
+      </div>
+      <footer>Sam Hu © 2023</footer>
+    </div>
   );
 }
 
