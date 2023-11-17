69fb630db2f91963f215624742a9aa3e11b7cb5c
Sam Hu <samhu@yahoo.com>
2023-11-17 00:48:05 -0500

    backto

--- src/Connections/ConnectionsNYTToday.tsx
+++ src/Connections/ConnectionsNYTToday.tsx
@@ -1,19 +1,22 @@
 import { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ConnectionsContext } from './ConnectionsContext';
 import { getTodayOffset, normalizeGame } from './utils';
 import ConnectionsPlay from './ConnectionsPlay';
 import LoadingSpinner from './Loading';
 
 function ConnectionsNYTToday() {
+  const location = useLocation();
   const { nytConnections, loadedConnections } = useContext(ConnectionsContext);
 
   const game = nytConnections[getTodayOffset()];
+  const backTo = location.state?.backTo || 'archive';
 
   if (!loadedConnections) {
     return <LoadingSpinner />;
   }
 
-  return <ConnectionsPlay game={normalizeGame(game, true)} backTo="archive" />;
+  return <ConnectionsPlay game={normalizeGame(game, true)} backTo={backTo} />;
 }
 
 export default ConnectionsNYTToday;
